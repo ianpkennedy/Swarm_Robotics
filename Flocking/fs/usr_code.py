74f1a4c4b01e81551774374cd2875317760eb5d9
Ian Kennedy <ian.p.kennedy.94@gmail.com>
2021-12-13 23:53:17 -0600

    fixing flocking code

--- Flocking/fs/usr_code.py
+++ Flocking/fs/usr_code.py
@@ -1,196 +1,274 @@
 """
+Lab 4 - Robot Flocking 
+
 Ian Kennedy
-Lab 3
-
-Simulation with coachbots emulating the brazilian nut effect.
-
-
-
-Simulation runs for 3 radii sorting took approximately 1500 simulated seconds till steady state
-Simulation runs for 2 radii sorting took approximately 700 simulated seconds till steady state
+ME409
 
 """
 
-
 def usr(robot):
-	import struct
-	import math
-	import timeit
-
-	#Random angle variable
-	ran = 0
+    import struct
+    import math
+    import timeit
 
 
-	#Net x, y and theta values
-	net_x = 0
-	net_y = 0
-	net_t = 0
-	
-	#robot pose data
-	g_x = 0
-	g_y = 0
-	g_a = 0
+	#Average position variables for cohesion effect
+    coh_x = 0.0
+    coh_y = 0.0
+    coh_o = 0.0
+    
+    coh_vec_x = 0.0
+    coh_vec_y = 0.0
+
+    v_r_coh = 0.5 #Virtual radius for cohesion sensing
+
+    r_a_coh = []
+    r_x_coh = []
+    r_y_coh = []
+    r_id_coh = []
+    same_coh = 0
+
+    #Average heading for alignment variables
+    avg_t = 0
+    avg_t_del = 0
+
+    #Lists for heading alignment
+    r_a_a = []
+    r_x_a = []
+    r_y_a = []
+    r_id_a = []
+
+    gain = 0
+    
+    #This is the alignment virtual radius. All robots should align to a global average heading
+    vr_a = 0.4
+
+    #Flag to check for identical robot messages in the lists for the repulsion and alignment affect
+    same = 0
+
+    #robot pose data
+    g_x = 0
+    g_y = 0
+    g_a = 0
 
 
 	#attraction vector to origin x,y components
-	net_x_o = 0
-	net_y_o = 0
+    mig_x = 0
+    mig_y = 0
+    mig_o = 0
+    dist_o = 0
 
-	#random vectors
-	net_x_r = 0
-	net_y_r = 0
+	#This is the virtual radius used for the robot avoidance maneuver
+    vr=0.3
 
-	# Data of a neighboring robots
-	dist = [] #distance from neighbor robot
-	d_a = [] # delta angle from neighbor robot
+	# Data of neighboring robots used for  
+
+    dist = [] #distance from neighbor robot
+    d_a = [] # delta angle from neighbor robot
 	
-	#Received data variables from neighboring robots
-	r_x = []
-	r_y = []
-	r_a = []
-	r_id = []
+    r_x = []
+    r_y = []
+    r_a = []
+    r_id = []
 
-	#Flag to check for identical robot messages in the lists
-	same = 0
+    net_x = 0
+    net_y = 0
+    net_t = 0
 
-	# Virtual radii for robots. The lower assigned ids have smaller radii
-	if robot.assigned_id==0:
-		robot.set_led(100,0,0)
-		vr=0.15
-	elif robot.assigned_id==1:
-		robot.set_led(0,100,0)
-		vr = 0.3	
-	else:
-		robot.set_led(0,0,100)
-		vr = 0.4
+    while True:
+        
+        #Acquire time, pose and phase variable for state behavior of robot
+        t = robot.get_clock()
+        pose = robot.get_pose()
+        phase = t % 5
+        
+        #Check for a pose, get x,y and theta data
+        if pose:
+            g_x = pose[0]
+            g_y = pose[1]
+            g_a = pose[2]
+            dist_o = math.sqrt((g_x-0.0)**2 + (g_y-0.0)**2) 
+            robot.send_msg(struct.pack('fffi', pose[0], pose[1],pose[2],robot.id)) #Send pose x,y in message
 
-		
-	while True:
-		
-
-		#Check time, position and reset flag filtering out identical messages
-		time = robot.get_clock()
-		pose = robot.get_pose()
-		same=0
-
-		
-		#The simulation operations in 5 second periods
-		time_mod = time%5
-
-		#Send data to other robots
-		if pose:
-			g_x = pose[0]
-			g_y = pose[1]
-			g_a = pose[2]
-			robot.send_msg(struct.pack('fffi', pose[0], pose[1],pose[2],robot.assigned_id))# send pose x,y in message
+        # Check for messages from neighbors
+        m = robot.recv_msg() 
 
 
-		#From 0 to 1 seconds, calculate heading and vector magnitude
-		if time_mod<1:
-			net_x = 0
-			net_y = 0
-			net_t = 0
+        # Collect migration force, cohesion force and repulsion force data in this state
+        if phase<1:
+            robot.set_vel(0,0)
+            robot.set_led(0,0,100)
 
-			
-			robot.set_vel(0,0)
+            mig_o = math.atan2(g_y-0,g_x-0) #heading to origin
+            mig_x = 1*math.cos(mig_o) #attraction to orgin vector components
+            mig_y = 1*math.sin(mig_o)
+            
+            # When received messages exist, add
+            if len(m)>0:
 
-			#Attraction vector
-			theta_o = math.atan2(g_y-0,g_x-0) #heading to origin
-			net_x_o = 1*math.cos(theta_o) #attraction to orgin vector components
-			net_y_o = 1*math.sin(theta_o)
+                # unpack received data
+                m_r = struct.unpack('fffi',m[0][:16])
 
-			#Random vector
-			ran = robot.random.randint(-314,314)
-			ran /= 100
+                #Filter out repeated data
+                for i in range(len(r_x)):
+                    if m_r[0]==r_x[i] and m_r[1]==r_y[i]:
+                        same=1
+                
+                for i in range(len(r_x_coh)):
+                    if m_r[0]==r_x_coh[i] and m_r[1]==r_y_coh[i]:
+                        same_coh=1
 
-			net_x_r = 1*math.cos(ran) #random vector components
-			net_y_r = 1*math.sin(ran)
+                #Exclude data outside of virtual communication range     
+                if same_coh==0 and math.sqrt((g_x - m_r[0])**2 + (g_y - m_r[1])**2) < v_r_coh:
+                    r_x_coh.append(m_r[0])
+                    r_y_coh.append(m_r[1])
+                    r_a_coh.append(m_r[2])
+                    r_id_coh.append(m_r[3])
 
-
-			#Check buffer for other messages
-			m = robot.recv_msg()
-
-			if len(m) > 0: #Update repulsion data
-				m_r = struct.unpack('fffi',m[0][:16])
-
-				for i in range(len(r_x)): # Filter out identical messages
-					if abs(m_r[0]-r_x[i])<0.1 and abs(m_r[1]-r_y[i])<0.1:
-						same=1
-				
-				#Append received data to the list
-				if same==0 and math.sqrt((g_x - m_r[0])*(g_x - m_r[0]) + (g_y - m_r[1])*(g_y - m_r[1])) < vr:
-					r_x.append(m_r[0])
-					r_y.append(m_r[1])
-					r_a.append(m_r[2])
-					r_id.append(m_r[3])			
-
-					#Calculate distances for each neighbor
-					dist_temp = math.sqrt((g_x - r_x[len(r_x)-1])**2 + (g_y - r_y[len(r_y)-1])**2)
-					dist.append(dist_temp)
-					
-					#Calculate the respective angles to neighbors
-					d_a_temp = math.atan2((m_r[1]-g_y),(m_r[0]-g_x)) - g_a
-
-					if d_a_temp>math.pi:
-						d_a_temp = -(2*math.pi-d_a_temp)
-					if d_a_temp<-math.pi:
-						d_a_temp = 2*math.pi+d_a_temp
-
-					d_a.append(d_a_temp)
-		
+                if same==0 and math.sqrt((g_x - m_r[0])**2 + (g_y - m_r[1])**2) < vr:
+                    r_x.append(m_r[0])
+                    r_y.append(m_r[1])
+                    r_a.append(m_r[2])
+                    r_id.append(m_r[3])
 
 
-		#Turn towards the calculated heading
-		if 1<time_mod<3:
-			
-			#Add the three separate vector influences (random motion, attraction to source, repulsion from neighbors)
-			net_x-=0.15*net_x_o
-			net_y-=0.15*net_y_o	
+                    #For repulsion, add calculate cartesian X,Y distance to neighbors
+                    dist_temp = math.sqrt((g_x - r_x[len(r_x)-1])**2 + (g_y - r_y[len(r_y)-1])**2)
+                    dist.append(dist_temp)
 
-			net_x-=0.1*net_x_r
-			net_y-=0.1*net_y_r		
+                    #Calculate heading angle with neighbors 
+                    d_a_temp = math.atan2((m_r[1]-g_y),(m_r[0]-g_x)) - g_a
+                    if d_a_temp>math.pi:
+                        d_a_temp = -(2*math.pi-d_a_temp)
+                    if d_a_temp<-math.pi:
+                        d_a_temp = 2*math.pi+d_a_temp
+                    d_a.append(d_a_temp)
 
-			for i in range(len(r_x)):
-				net_x+=0.3*(g_x-r_x[i])/dist[i]
-				net_y+=0.3*(g_y-r_y[i])/dist[i]			
-
-			#Calculate heading		
-			try:
-				net_t = math.atan2(net_y,net_x)-g_a
-			except:
-				pass
-
-			if net_t>math.pi:
-				net_t -= 2*math.pi
-			if net_t<-math.pi:
-				net_t += 2*math.pi
-			
-			try: # Go to heading. Don't set velocity if net_t=0
-				robot.set_vel( -int(99*abs(net_t)/net_t) ,+int(99*abs(net_t)/net_t))
-			except:
-				pass
+            #Reset flags detecting repetitive messages
+            same = 0
+            same_coh = 0
 
 
-		#Do forward propulsion based off of neighbor data.
-		if 3<time_mod<4:
-			if abs(net_x)>0 or abs(net_y)>0:
-				robot.set_vel(math.ceil(math.sqrt(net_x*net_x + net_y*net_y)),math.ceil(math.sqrt(net_x*net_x + net_y*net_y)))
-			if net_y==0 and net_x==0:
-				pass
+            # The calculate average location of neighbors
+            if len(r_x_coh)>0:
+                coh_x = sum(r_x_coh)/len(r_x_coh)
+                coh_y = sum(r_y_coh)/len(r_y_coh)
+                coh_o = math.atan2(g_y-coh_y,g_x-coh_x) 
+                coh_vec_x = 1*math.cos(coh_o)
+                coh_vec_y = 1*math.sin(coh_o) #Calculate the cohesion force X,Y components
 
-		#Clear the neighbor data lists
-		if time_mod>4:
-			robot.set_vel(0,0)
-			r_x=[]
-			r_y=[]
-			r_a=[]
-			r_id=[]
-			dist=[]
-			d_a=[]			
+
+#after moving forward you could calculate a new heading agv t delta and do a forward move based off of that before start of new cycle
+#try making the alignment virtual radius smaller
 
 
 
 
 
+        if 1<phase<2:
+            robot.set_led(100,100,0)
 
-		
+            net_x=-0.03*dist_o*mig_x 
+            net_y=-0.03*dist_o*mig_y	
+
+            net_x-=0.02*coh_vec_x
+            net_y-=0.02*coh_vec_y
+
+            for i in range(len(r_x)):
+                net_x+=0.01*(g_x-r_x[i])/dist[i]
+                net_y+=0.01*(g_y-r_y[i])/dist[i]
+
+            #Calculate heading
+            try:
+                net_t = math.atan2(net_y,net_x) - g_a
+            except:
+                pass
+            
+            if net_t>math.pi:
+                net_t -= 2*math.pi
+            if net_t<-math.pi:
+                net_t += 2*math.pi    
+
+
+            #Go to heading
+            turn = int(99*abs(net_t)/net_t)
+            try: # Go to heading. Don't set velocity if net_t=0. scale by dist_0
+                robot.set_vel( -turn ,+turn)
+            except:
+                pass
+
+        # In this phase, the robots move towards their target 
+        if 2.0<phase<3.0:
+            robot.set_led(0,100,100)
+            robot.set_vel(int(1000*dist_o*math.sqrt(net_x*net_x + net_y*net_y)),int(1000*dist_o*math.sqrt(net_x*net_x + net_y*net_y)) )
+
+
+        # Calculate average heading alignment for robots
+        if 3.0<phase<4.0:
+            robot.set_led(0,100,0)
+            robot.set_vel(0,0)
+            
+
+            #Check for new messages
+            if len(m)>0:
+                m_r = struct.unpack('fffi',m[0][:16])
+                
+                #Filter out repetitive messages
+                for i in range(len(r_x_a)):
+                    if m_r[0]==r_x_a[i] and m_r[1]==r_y_a[i]:
+                        same=1
+
+                #Append new data
+                if same==0 and math.sqrt((g_x - m_r[0])**2 + (g_y - m_r[1])**2) < vr_a:
+                    r_x_a.append(m_r[0])
+                    r_y_a.append(m_r[1])                    
+                    r_a_a.append(m_r[2])
+                    r_id.append(m_r[3])
+
+            same  = 0
+            
+            #Calculate average heading
+            if len(r_a_a)>0:
+                try:
+                    avg_t = sum(r_a_a)/len(r_a_a)
+                except:
+                    avg_t = 0
+
+            #Calculate average delta angle from average heading
+            avg_t_del =  avg_t - g_a    
+                                        
+        #Implement heading alignment between robots
+        if 4.0<phase<4.9:
+            robot.set_led(100,100,100)
+            gain = abs(int(150*avg_t_del))
+
+
+            #Check which direction to align to
+            if avg_t_del>0.1:
+                robot.set_vel(5,5+gain)
+
+            elif avg_t_del<-0.1:
+                robot.set_vel(5+gain,5)
+            else:
+                robot.set_vel(5,5)
+
+        if phase>4.9:
+            #Clear all the data
+            net_x = 0
+            net_y = 0
+            net_t=0
+            r_x=[]
+            r_y=[]
+            r_a=[]
+            r_id=[]
+            dist=[]
+            d_a=[]
+            r_a_a=[]
+            r_x_a=[]
+            r_y_a=[]
+            r_x_coh=[]
+            r_y_coh=[]
+            r_a_coh=[]
+            r_id_coh=[]
+
+          
